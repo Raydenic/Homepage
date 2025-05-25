5a6ea935f92a4e7be8b9aa8fdfd01567a89608eb
Mustafa Khan <mm5khan@uwaterloo.ca>
2025-05-25 16:52:30 -0400

    Looks updated V0

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import ejs from "ejs"
 const app=express();
 const port=process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
 app.listen(port,()=>{
     console.log(`Server Running on port ${port}`);
 })
